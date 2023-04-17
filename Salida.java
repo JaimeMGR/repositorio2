94c03a97be67ed8bd23a4d41c336a91914256116
JaimeMGR <123658735+JaimeMGR@users.noreply.github.com>
2023-04-17 12:34:19 +0200

    Create Salida.java

---
+++ Salida.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.FileOutputStream;
+
+public class Salida {
+    void escribir() {
+        byte CR = 13;
+        byte LF = 10;
+        try {
+
+
+            FileOutputStream fos = new FileOutputStream("c:\\ficheros\\fich1.bin");
+        }
+    }
+}
