77e9afeead9d27aeb93bad1ee60d030ae12234fa
JaimeMGR <123658735+JaimeMGR@users.noreply.github.com>
2023-04-17 13:56:19 +0200

    Update Salida.java

--- Salida.java
+++ Salida.java
@@ -1,15 +1,27 @@
 package org.example;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-
+import java.io.IOException;
 public class Salida {
-    void escribir() {
+    static void escribir() {
         byte CR = 13;
         byte LF = 10;
+
         try {
-
-
-            FileOutputStream fos = new FileOutputStream("c:\\ficheros\\fich1.bin");
+            FileOutputStream fos = new FileOutputStream("fich1.bin");
+            fos.write((byte)'a');
+            fos.write((byte)'e');
+            fos.write((byte)'i');
+            fos.write(CR);
+            fos.write(LF);
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
         }
     }
+
+
 }
